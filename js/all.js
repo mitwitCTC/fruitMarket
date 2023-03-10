f7c8a8319b85c7d8e31f3eb55dabce93469bc85e
Eileen-Qian <107092198+Eileen-Qian@users.noreply.github.com>
2023-03-10 16:30:43 +0800

    update user
    user 資料取得、新增、修改、刪除

--- js/all.js
+++ js/all.js
@@ -1,28 +1,41 @@
 import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js';
 
-const API = 'https://64a7-114-32-150-22.ap.ngrok.io';
+const API = 'https://af97-114-32-150-22.ap.ngrok.io';
 
 // 用戶車籍
 const getMembersApi = `${API}/member/memberInfo`;
 let memberModal = null;
 let delMemberModal = null;
-const loginCheckApi = `${API}/user/loginCheck`;
+
+// 使用者
+const getUsersApi = `${API}/users/userInfo`;
+let userModal = null;
+let delUserModal = null;
 
 createApp({
     data() {
         return {
             today: new Date().toLocaleString(),
-            personnelcode: '',
+            // loginData: {
+            //     account: '',
+            //     token: ''
+            // },
+            // 用戶車籍
             members: [],
             tempMember: {},
-            isNewMember: false, // 用來確認是新增或是編輯
-            searchResult: [], //搜尋結果陣列，初始預設為空陣列
-            searchdata: '' //搜尋關鍵字，初始預設為空字串
+            isNewMember: false, // 用來確認是新增或是編輯用戶車籍
+            // searchResult: [], //搜尋結果陣列，初始預設為空陣列
+            searchdata: '', //搜尋關鍵字，初始預設為空字串
+            // 使用者
+            users: [],
+            tempUser: {},
+            isNewUser: false //用來確認是新增或編輯使用者
         }
     },
     methods: {
         // 確認登入
         // loginCheck(account, userToken) {
+        //     const loginCheckApi = `${API}/user/loginCheck`;
         //     axios
         //         .post(`${loginCheckApi}`)
         //         .then((response) => {
@@ -31,14 +44,28 @@ createApp({
         //         })
         //         .catch((error) => {
         //             alert(error);
-        //             window.location = `login.html`;
+        //             // window.location = `login.html`;
         //         })
         // },
 
+        // logout
+        loginOut() {
+            const logoutApi = `${API}/users/logout`;
+            axios
+                .post(logoutApi)
+                .then(() => {
+                    document.cookie = "userToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC;";
+                    window.location = `login.html`;
+                })
+                .catch((error) => {
+                    alert(error.message);
+                })
+
+        },
         // 取得用戶車籍資料 (members)
         getMembers() {
             axios
-                .get(`${getMembersApi}`)
+                .get(getMembersApi)
                 .then((response) => {
                     // console.log(response.data);
                     this.members = response.data;
@@ -46,7 +73,6 @@ createApp({
                     alert(error);
                 })
         },
-
         // 開啟新增/編輯用戶車籍(member) modal
         openMemberModal(status, member) {
             if (status === 'create') {
@@ -72,7 +98,7 @@ createApp({
             let method = 'post';
             if (!this.isNewMember) {
                 updateMemberApi = `${API}/member/updateMember/${this.tempMember.userId}`;
-                method = 'put'
+                method = 'put';
             }
             axios
             [method](updateMemberApi, { target: this.tempMember })
@@ -102,7 +128,7 @@ createApp({
         searchMember(searchdata) {
             const searchMemberApi = `${API}/member/searchMember`;
             axios
-                .post(searchMemberApi, { target: {searchdata:this.searchdata} })
+                .post(searchMemberApi, { target: { searchdata: this.searchdata } })
                 .then((response) => {
                     // console.log(response.data);
                     // console.log(this.searchdata);
@@ -111,15 +137,97 @@ createApp({
                 .catch((error) => {
                     alert(error);
                 })
+        },
+        // 取得使用者資料
+        getUsers() {
+            axios
+                .get(getUsersApi)
+                .then((response) => {
+                    // console.log(response.data);
+                    this.users = response.data;
+                })
+                .catch((error) => {
+                    alert(error);
+                })
+
+        },
+        // 開啟新增/修改使用者 (user) modal
+        openUserModal(status, user) {
+            if (status === 'create') {
+                userModal.show();
+                this.isNewUser = true;
+                // 新增時會帶入初始資料
+                this.tempUser = {};
+            } else if (status === 'edit') {
+                userModal.show();
+                this.isNewUser = false;
+                // 編輯的話，會帶入當前要編輯的原資料
+                this.tempUser = { ...user };
+                // console.log(this.tempUser);
+            }
+        },
+        // 開啟刪除使用者 (user) modal
+        openDelUserModal(user) {
+            delUserModal.show();
+            this.tempUser = { ...user };
+        },
+        // 新增/修改使用者
+        updateUser() {
+            let updateUserApi = `${API}/users/createUser`;
+            let method = 'post';
+            if (!this.isNewUser) {
+                updateUserApi = `${API}/users/updateUser/${this.tempUser.id}`;
+                method = 'put';
+                if (this.tempUser.newPassword !== this.tempUser.newPasswordCheck) {
+                    alert("新密碼輸入不一致");
+                    return;
+                } else {
+                    this.tempUser.password = this.tempUser.newPassword;
+                }
+                // console.log(this.tempUser);
+            };
+            axios
+            [method](updateUserApi, { target: this.tempUser })
+                .then((response) => {
+                    if (method === 'put') {
+                        alert("修改成功～請重新登入");
+                        document.cookie = "userToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC;";
+                        window.location = `login.html`;
+                    };
+                    alert(response.data.message);
+                    userModal.hide();
+                    this.getUsers(); //新增後重新取得用戶車籍列表
+                })
+                .catch((error) => {
+                    alert(error)
+                })
+        },
+        // 刪除使用者
+        deleteUser() {
+            const deleteUserApi = `${API}/users/deleteUser/${this.tempUser.id}`;
+            axios
+                .patch(deleteUserApi, { target: this.tempUser.id })
+                .then(() => {
+                    this.getUsers(); //刪除後重新取得使用者資料列表
+                    delUserModal.hide();
+                    document.cookie = "userToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC;";
+                    window.location = `login.html`;
+                })
+                .catch((error) => {
+                    alert(error);
+                })
         }
+        // 使用者
     },
     mounted() {
-        // const userToken = '';
         // axios.default.headers.common['Authorization'] = userToken;
         // this.loginCheck();
         this.getMembers();
+        this.getUsers();
         // 初始化 bootstrap modal
         memberModal = new bootstrap.Modal('#memberModal');
         delMemberModal = new bootstrap.Modal('#delMemberModal');
+        userModal = new bootstrap.Modal('#userModal');
+        delUserModal = new bootstrap.Modal('#delUserModal');
     }
 }).mount('#app');
